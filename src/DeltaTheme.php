f60dd81e19dc88a3e3c993bf37163303889e473f
Ryan Thompson <service@aiwebsystems.com>
2015-06-21 04:23:41 -0500

    Initial

---
+++ src/DeltaTheme.php
@@ -0,0 +1,16 @@
+<?php namespace Anomaly\DeltaTheme;
+
+use Anomaly\Streams\Platform\Addon\Theme\Theme;
+
+/**
+ * Class DeltaTheme
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\DeltaTheme
+ */
+class DeltaTheme extends Theme
+{
+
+}
